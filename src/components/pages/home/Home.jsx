febad7a56db8708affc13017ec1a1d4550bbf238
abdulalh-al-mohammad <abdullahalmuhammad6@mail.com>
2024-10-30 15:08:08 +0600

    add header

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,28 +1,35 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, A11y } from 'swiper/modules';
+import { Navigation, Pagination, A11y, EffectFade, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+import 'swiper/css/autoplay';
 import banner1 from '../../../assets/banner/banner1.jpg'
 import banner2 from '../../../assets/banner/banner2.jpg'
 import banner3 from '../../../assets/banner/banner3.jpg'
+import Header from '../../../layout/header/Header';
 
 const Home = () => {
     return (
-        <Swiper
-            modules={[Navigation, Pagination, A11y]}
-            spaceBetween={50}
-            slidesPerView={0}
-            navigation
-            pagination={{ clickable: true }}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-        >
-            <SwiperSlide><img src={banner1} alt="" /></SwiperSlide>
-            <SwiperSlide><img src={banner2} alt="" /></SwiperSlide>
-            <SwiperSlide><img src={banner3} alt="" /></SwiperSlide>
-            ...
-        </Swiper>
+        <div>
+            <Swiper
+                className='h-96 w-full'
+                modules={[Navigation, Pagination, A11y, EffectFade, Autoplay]}
+                spaceBetween={50}
+                slidesPerView={1}
+                effect='fade'
+                navigation
+                autoplay
+                pagination={{ clickable: true }}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide><img className='h-full object-cover' src={banner2} alt="" /></SwiperSlide>
+                <SwiperSlide><img className='h-full object-cover' src={banner3} alt="" /></SwiperSlide>
+                <SwiperSlide><img className='h-full object-cover' src={banner1} alt="" /></SwiperSlide>
+            </Swiper>
+        </div>
     );
 };
 
--- src/layout/main/Main.jsx
+++ src/layout/main/Main.jsx
@@ -5,7 +5,7 @@ import Footer from '../footer/Footer';
 
 const Main = () => {
     return (
-        <div className='container mx-auto'>
+        <div>
             <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
